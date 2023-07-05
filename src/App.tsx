1f3442878c118e8800d08575fcc21a275165fbe4
Matheus Alves <mtalves27@gmail.com>
2023-07-05 00:51:36 -0300

    initial commit ✨

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { AddTodo } from "./components/AddTodo";
+import { TodoList } from "./components/TodoList";
+
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./store";
+
+function App() {
+  return (
+    <ReduxProvider store={store}>
+      <TodoList />
+      <AddTodo />
+    </ReduxProvider>
+  );
+}
+
+export default App;
