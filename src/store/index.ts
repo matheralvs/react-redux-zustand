8882852f0515a2ed0ea286af48925f0ee08ff10f
Matheus Alves <mtalves27@gmail.com>
2023-07-05 01:11:07 -0300

    chore: creating and firing actions in redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,16 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+import { TypedUseSelectorHook, useSelector } from "react-redux";
+
 const todoSlice = createSlice({
   name: "todo",
   initialState: ["Fazer cafe", "Estudar redux"],
 
-  reducers: {},
+  reducers: {
+    add: (state, action) => {
+      state.push(action.payload.newTodo);
+    },
+  },
 });
 
 export const store = configureStore({
@@ -12,3 +18,9 @@ export const store = configureStore({
     todo: todoSlice.reducer,
   },
 });
+
+export const { add } = todoSlice.actions;
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
